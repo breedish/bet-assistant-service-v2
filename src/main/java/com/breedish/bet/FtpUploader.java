451b6466e73880f3751283e65928b9fcc9dfa118
dzenin <dzenin@edmunds.com>
2016-11-14 21:57:40 +0300

    Add timer

--- src/main/java/com/breedish/bet/FtpUploader.java
+++ src/main/java/com/breedish/bet/FtpUploader.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,15 +23,18 @@ public class FtpUploader {
     private BetProperties betProperties;
 
     public void uploadData(InputStream is, String filename) {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        LOG.info("Start upload of {0}", filename);
+        System.out.println(filename);
         FTPClient client = new FTPClient();
         try {
             client.connect(betProperties.getFtpServer());
             client.login(betProperties.getFtpUser(), betProperties.getFtpPassword());
-            client.storeFile(
-                new File(betProperties.getFtpDataDirectory(), filename).toString(), is
-            );
+            client.storeFile(new File(betProperties.getFtpDataDirectory(), filename).toString(), is);
             client.logout();
         } catch (IOException e) {
+            System.out.println(e);
             LOG.error("Issue uploading file {0}. Reason {1}", filename, e);
         } finally {
             try {
@@ -39,5 +43,7 @@ public class FtpUploader {
                 LOG.error("Issue uploading file {0}. Reason {1}", filename, e);
             }
         }
+        watch.stop();
+        LOG.info("Upload of {0} was done in {1}", filename, watch.getLastTaskInfo().getTimeMillis());
     }
 }
